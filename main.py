5359d569507b698b4f10fde64a8a23db9d4bf0b8
yh119 <51743974+word119@users.noreply.github.com>
2025-08-22 16:40:07 +0200

    Environment Variable for security

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-#app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+#app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
